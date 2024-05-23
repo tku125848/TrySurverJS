b688ff4a6c5cec914dde76f9d9b945f494e32c68
曾江安 <125848@o365.tku.edu.tw>
2024-05-23 08:50:45 +0800

    加入專案檔案。

---
+++ wwwroot/lib/json/campus.js
@@ -0,0 +1,107 @@
+const aachoices =
+    [
+        {
+            "value": "A",
+            "text": "A 場地"
+        },
+        {
+            "value": "B",
+            "text": "B 場地"
+        },
+        {
+            "value": "C",
+            "text": "C 場地"
+        },
+        {
+            "value": "CL",
+            "text": "CL 場地"
+        },
+        {
+            "value": "D",
+            "text": "D 場地"
+        },
+        {
+            "value": "E",
+            "text": "E 場地"
+        },
+        {
+            "value": "ED",
+            "text": "ED 場地"
+        },
+        {
+            "value": "F",
+            "text": "F 場地"
+        },
+        {
+            "value": "FL",
+            "text": "FL 場地"
+        },
+        {
+            "value": "G",
+            "text": "G 場地"
+        },
+        {
+            "value": "H",
+            "text": "H 場地"
+        },
+        {
+            "value": "HC",
+            "text": "HC 場地"
+        },
+        {
+            "value": "I",
+            "text": "I 場地"
+        },
+        {
+            "value": "J",
+            "text": "J 場地"
+        },
+        {
+            "value": "L",
+            "text": "L 場地"
+        },
+        {
+            "value": "M",
+            "text": "M 場地"
+        },
+        {
+            "value": "N",
+            "text": "N 場地"
+        },
+        {
+            "value": "O",
+            "text": "O 場地"
+        },
+        {
+            "value": "Q",
+            "text": "Q 場地"
+        },
+        {
+            "value": "R",
+            "text": "R 場地"
+        },
+        {
+            "value": "S",
+            "text": "S 場地"
+        },
+        {
+            "value": "SG",
+            "text": "SG 場地"
+        },
+        {
+            "value": "T",
+            "text": "T 場地"
+        },
+        {
+            "value": "U",
+            "text": "U 場地"
+        },
+        {
+            "value": "V",
+            "text": "V 場地"
+        },
+        {
+            "value": "Z",
+            "text": "Z 場地"
+        }
+    ];
